5b1d9428b09137b6dda3f48325bf2658bdd34b6b
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-07-19 23:06:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+//your JS code here. If required
+let name = prompt("Enter a string")
+const freq = [];
+for(let i=0; i < name.length;i++){
+	
+	const char = name[i];	
+	// console.log(char)
+	freq[char] = (freq[char]||0)+1;
+}
+// console.log(freq);
+
+for(let i=0;i<name.length;i++){
+	const char = name[i];
+	if(freq[char]==1){
+		alert(char);
+	}
+}
+
